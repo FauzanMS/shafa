2056d7b3f394de023d5128a377c6973be6144c3d
unknown <fauzan4mak@gmail.com>
2021-10-07 18:50:21 +0530

    Third commit

--- src/Circle.js
+++ src/Circle.js
@@ -3,7 +3,7 @@ import './Circle.css'
 export default function Circle(props) {
     return (
 <div>
-        <div className="circle" style={{ background: `conic-gradient( purple ${props.valuepercent}%  , white 10% )` }}>
+        <div className="circle" style={{ background: `conic-gradient( purple ${props.valuepercent}%  , white 1% )` }}>
                   <div className="innercircle">
               <p className="value">{props.value}</p>
             </div>  
