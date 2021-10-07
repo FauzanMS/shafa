a29c0b78a2d1afa56134e6dd2a83ae98b8714117
unknown <fauzan4mak@gmail.com>
2021-10-07 18:48:09 +0530

    second commit

--- src/SkillsCharts.js
+++ src/SkillsCharts.js
@@ -7,7 +7,7 @@ export default function SkillsCharts(props) {
         <p className="heading2">{props.skill} - {props.method}</p>
         <div className="grid_second">
           <Circle value = "6.67" title="MAX" valuepercent="67"/>
-          <Circle value="46" title="AVG" valuepercent="42" />
+          <Circle value="46" title="AVG" valuepercent="47" />
         </div>
     </div>
   );
